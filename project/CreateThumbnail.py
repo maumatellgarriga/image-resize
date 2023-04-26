283a7e78e56d14088ad4c86acc329ce1159c782b
Miriam <m.aumatell33@gmail.com>
2023-04-26 15:54:53 +0200

    final changes

--- project/CreateThumbnail.py
+++ project/CreateThumbnail.py
@@ -3,25 +3,29 @@ from PIL import Image
 import urllib.parse
 import os
 
+s3_resource = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
 def resize_image(image_path, resized_path):
     with Image.open(image_path) as image:
-        image.thumbnail((128, 128))
+        print("start resize...")
+        image.thumbnail((350, 350))
+        print("...resized")
+        if not os.path.exists(os.path.dirname(resized_path)):
+            os.makedirs(os.path.dirname(resized_path))
         image.save(resized_path)
-     
+
 def handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = urllib.parse.unquote_plus(record['s3']['object']['key'], encoding='utf-8')
         download_path = '/tmp/{}'.format(key)
         upload_path = '/tmp/resized-{}'.format(key)
-        print(bucket)
-        print(key)
-        print(download_path)
-        print(upload_path)
+        
         if not os.path.exists(os.path.dirname(download_path)):
             os.makedirs(os.path.dirname(download_path))
-        #s3_client.download_file(bucket, key, download_path)
-        #resize_image(download_path, upload_path)
-        #s3_client.upload_file(upload_path, '{}-resized'.format(bucket), key)
+        s3_resource.meta.client.download_file(bucket, key, download_path)
+        resize_image(download_path, upload_path)
+        upload_bucket = '{}-resized'.format(bucket)
+        upload_key = key
+        s3_client.upload_file(upload_path, upload_bucket, upload_key)
